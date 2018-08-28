302cb6e4ffd6e5dfc47e7aaa4e675122913cae6d
Kunal Shukla <kunal.shukla@intact.net>
2018-08-28 16:45:46 -0400

    MongoDB connection error

--- hotelReservation/server/routes/api.ts
+++ hotelReservation/server/routes/api.ts
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://kunal:kunal5@ds227322.mlab.com:27322/usersinfo";
+var db = MongoClient(url, ['users']);
 
 router.get('/', function(req,res,next){
-    res.send('api works');
+    console.log('api works');
+    db.users.find(function(err, users){
+    if(err){
+        res.send(err);     
+    }
+    else{
+        console.log("in here");
+        res.json(users);
+        res.send('in here');
+    }
+    });
 });
 
 module.exports = router;
