fb50a9d3acac8d022612cb34286acb7023514e7c
Kunal Shukla <kunal.shukla@intact.net>
2018-08-30 11:41:14 -0400

    final changes to api,ts

--- hotelReservation/server/routes/api.ts
+++ hotelReservation/server/routes/api.ts
@@ -4,19 +4,17 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var mongojs = require('mongojs');
 var url = "mongodb://kunal:kunal5@ds227322.mlab.com:27322/usersinfo";
-//var assert = require('assert');
 var db = mongojs(url, ['users']);
-//MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
-    //assert.equal(null, err);
-    router.get('/', function(req,res,next){
-        db.users.find((err, users) => {
-            if(err){
-                res.send(err);
-            }
-            else{
-                res.json(users);
-            }
-        });
+
+router.get('/', function(req,res,next){
+    db.users.find((err, users) => {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.json(users);
+        }
     });
+});
 module.exports = router;
  
