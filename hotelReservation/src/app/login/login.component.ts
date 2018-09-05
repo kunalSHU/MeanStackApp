09f9a7a029fbb4faa8a79563905c840d76d503dd
Kunal Shukla <kunal.shukla@intact.net>
2018-09-05 16:48:17 -0400

    working on register page

--- hotelReservation/src/app/login/login.component.ts
+++ hotelReservation/src/app/login/login.component.ts
@@ -7,17 +7,6 @@ import {NgProgress} from '@ngx-progressbar/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- /* loading = true;
-  constructor(private router: Router) { 
-    this.router.events.subscribe((event:Event) => {
-      if(event instanceof NavigationStart){
-        this.loading = true;
-      }
-      if(event instanceof NavigationEnd){
-        this.loading = false;
-      }
-    });
-  }*/
   ngOnInit() {
   }
   register(){
