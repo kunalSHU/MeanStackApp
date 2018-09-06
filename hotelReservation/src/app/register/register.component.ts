04c80e4d791baff8df70c25e1f73a3db8b013668
Kunal Shukla <kunal.shukla@intact.net>
2018-09-06 11:29:36 -0400

    multi step form working

--- hotelReservation/src/app/register/register.component.ts
+++ hotelReservation/src/app/register/register.component.ts
@@ -17,51 +17,14 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
-  nextCall(){
-    
-    if(this.animating){
-      return false;
-    }
-    this.animating = true;
+  firstnextCall(){
+    $("#msform").children("#f1").hide();
+    this.current_fs = $(this).parent();
+    this.next_fs = $(this).parent().next();
+    $("#progressbar li").eq(1).addClass("active");
+    $("#msform").children("#f2").show();
+  }
+  secondnextCall(){
 
-    //gets the current field set
-    this.current_fs =  $(this).parent();
-    //gets the next field set
-    this.next_fs =  $(this).parent().next();
-    console.log(this.current_fs);
-    
-    console.log(this.next_fs);
-    console.log('next was clicked');
-
-    //activate next step on progressbar using the index of next_fs
-    console.log($("fieldset").index(this.current_fs));
-    console.log($("fieldset").index(this.next_fs));
-    $("#progressbar li").eq($("fieldset").index(this.next_fs)).addClass("active");
-
-    this.next_fs.show();
-    	//hide the current fieldset with style
-	  this.current_fs.animate({opacity: 0}, {
-      step: function(now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale current_fs down to 80%
-        this.scale = 1 - (1 - now) * 0.2;
-        //2. bring next_fs from the right(50%)
-        this.left = (now * 50)+"%";
-        //3. increase opacity of next_fs to 1 as it moves in
-        this.opacity = 1 - now;
-        this.current_fs.css({
-          'transform': 'scale('+this.scale+')',
-          'position': 'absolute'
-        });
-        this.next_fs.css({'left': this.left, 'opacity': this.opacity});
-      }, 
-      duration: 800, 
-      complete: function(){
-        this.current_fs.hide();
-        this.animating = false;
-      }, 
-      //this comes from the custom easing plugin
-      easing: 'easeInOutBack'
-    });    
   }
 }
