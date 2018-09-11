5a9e7dc92f745cf1196d8a5a5a293f2daa7125af
Kunal Shukla <kunal.shukla@intact.net>
2018-09-11 11:45:11 -0400

    working on registering user info

--- hotelReservation/src/app/register/register.component.ts
+++ hotelReservation/src/app/register/register.component.ts
@@ -30,52 +30,6 @@ export class RegisterComponent implements OnInit {
   isSelectedStep2 = false;
   private control:AbstractControl;
 
-  //Regex for patterns
-  emailPattern = "^[a-z]+@[a-z]+[.](com|ca)$";
-  namePattern = "^[a-zA-Z]{2,13}$";
-  numberPattern = "^[0-9]{10,15}$";
-
-  //Required Validators
-  emailFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-
-  passwordFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-
-  confirmpasswordFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-  firstNameFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-  lastNameFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-  dateBirthFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-  numberControl = new FormControl('', [
-    Validators.required,
-  ]);
-  streetControl = new FormControl('', [
-    Validators.required,
-  ]);
-  postalCodeControl = new FormControl('', [
-    Validators.required,
-  ]);
-
-  matcher = new MyErrorStateMatcher();
-
-  //storing the chip buttons to be used in a list
-  availableColors: ChipColor[] = [
-    {name: 'none', color: undefined},
-    {name: 'Next', color: 'primary'},
-    {name: 'Accent', color: 'accent'},
-    {name: 'Warn', color: 'warn'}
-
-  ];
 
   animating: any;
   current_fs: any;
@@ -91,18 +45,44 @@ export class RegisterComponent implements OnInit {
   account = new accountSetup();
   personal = new personalDetails();
 
+  //Regex for patterns
+  emailPattern = "^[a-z]+@[a-z]+[.](com|ca)$";
+  namePattern = "^[a-zA-Z]{2,13}$";
+  numberPattern = "^[0-9]{10,15}$";
+
+  //Required Validators
+  emailFormControl = new FormControl('');
+  passwordFormControl = new FormControl('');
+  confirmpasswordFormControl = new FormControl('');
+  firstNameFormControl = new FormControl('');
+  lastNameFormControl = new FormControl('');
+  dateBirthFormControl = new FormControl('');
+  numberControl = new FormControl('');
+  streetControl = new FormControl('');
+  postalCodeControl = new FormControl('');
+
+  matcher = new MyErrorStateMatcher();
+
+  //storing the chip buttons to be used in a list
+  availableColors: ChipColor[] = [
+    {name: 'none', color: undefined},
+    {name: 'Next', color: 'primary'},
+    {name: 'Accent', color: 'accent'},
+    {name: 'Warn', color: 'warn'}
+
+  ];
   
-//custom validator test
-emailPatternValidator(pattern: any): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: boolean } | null => {
-      console.log('in the validator');
-      if (control.value == pattern) {
-        console.log('email matches pattern');
-          return { 'ageRange': true };
-      }
-      return null;
-  };
-}
+  //custom validator test
+  emailPatternValidator(pattern: any): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+        console.log('in the validator');
+        if (control.value == pattern) {
+          console.log('email matches pattern');
+            return { 'ageRange': true };
+        }
+        return null;
+    };
+  }
   constructor(public snackBar: MatSnackBar) { }
 
   //this is where we take user data and make an ajax request to the db
@@ -113,15 +93,17 @@ emailPatternValidator(pattern: any): ValidatorFn {
   }
 
   ngOnInit() {
+
+    $("#submitButton").hide();
     console.log('hellot');
     console.log(this.emailFormControl.errors);
   }
   validateFirstnext(email: boolean, requiredEmail: boolean, requiredPassword: boolean, requiredConfirmpassword: boolean){
-    console.log(email);
+    /*console.log(email);
     console.log(requiredEmail);
     console.log(requiredPassword);
-    console.log(requiredConfirmpassword);
-    
+    console.log(requiredConfirmpassword);*/
+      
     if(this.passwordFormControl.value != this.confirmpasswordFormControl.value){
       this.isSelectedStep1 = false;
     }
@@ -147,7 +129,7 @@ emailPatternValidator(pattern: any): ValidatorFn {
     telephonePattern: boolean,streetrequired:boolean, 
     coderequired: boolean){
 
-      console.log(firstNamerequired);
+      /*console.log(firstNamerequired);
       console.log(firstNamePattern);
       console.log(lastNamerequired);
       console.log(lastNamePattern);
@@ -155,23 +137,23 @@ emailPatternValidator(pattern: any): ValidatorFn {
       console.log(telephonerequired);
       console.log(telephonePattern);
       console.log(streetrequired);
-      console.log(coderequired);
-      console.log(firstNamePattern==null);
+      console.log(coderequired);*/
       if(!(!firstNamerequired && (firstNamePattern==null) && !lastNamerequired && (lastNamePattern==null) &&
       !dateOfBirthrequired && !telephonerequired && (telephonePattern==null) && !streetrequired && !coderequired)){
-        console.log("IN HERE");
-        this.isSelectedStep1 = false;
+        this.isSelectedStep2 = false;
       }
       else if(this.isSelectedStep2){
         this.secondnextCall();
       }
       else if(!firstNamerequired && (firstNamePattern==null) && !lastNamerequired && (lastNamePattern==null) &&
         !dateOfBirthrequired && !telephonerequired && (telephonePattern==null) && !streetrequired && !coderequired){
-          console.log("IN HERE 2S");
           this.isSelectedStep2 = true;
       }
   }
-    
+  registerSubmit(){
+    console.log('in register submit function');
+  }
+
   firstnextCall(){
     //Using JQuery for the progress bar 
     $("#msform").children("#f1").hide();
@@ -186,6 +168,7 @@ emailPatternValidator(pattern: any): ValidatorFn {
     this.next_fs = $(this).parent().next();
     $("#progressbar li").eq(2).addClass("active");
     $("#msform").children("#f3").show();
+    $("#submitButton").show();
   }
   firstprevCall(){
     //Using JQuery for the progress bar 
@@ -198,6 +181,7 @@ emailPatternValidator(pattern: any): ValidatorFn {
   secondprevCall(){
     //Using JQuery for the progress bar 
     $("#msform").children("#f3").hide();
+    $("#submitButton").hide();
     this.current_fs = $(this).parent();
     this.next_fs = $(this).parent().prev();
     $("#progressbar li").eq(2).removeClass("active");
