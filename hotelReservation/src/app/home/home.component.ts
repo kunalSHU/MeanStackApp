95981243e986a753b185ec3a84cacf24c5688a7a
Kunal Shukla <kunal.shukla@intact.net>
2018-09-27 16:35:01 -0400

    working on Login IO

--- hotelReservation/src/app/home/home.component.ts
+++ hotelReservation/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
 import {Router} from '@angular/router';
+import {MapsAPILoader} from '@agm/core';
+import {NgForm, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,22 @@ import {Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  autocomplete:any;
+  locationControl = new FormControl();
+
+  //Search functionality using google maps API
+  @ViewChild('search') public searchElement: ElementRef;
+
+  constructor(private router: Router,private mapsAPILoader: MapsAPILoader) { }
 
   ngOnInit() {
     localStorage.setItem('homeUrl', this.router.url);
+    this.mapsAPILoader.load().then(()=> {
+      this.autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement, { types:["(cities)"] });
+    });
+  }
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 
 }
