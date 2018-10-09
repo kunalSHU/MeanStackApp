7c555d4c2f1ae19ca79b63195fefb151511f970c
Kunal Shukla <kunal.shukla@intact.net>
2018-10-09 14:52:35 -0400

    finding cuisine from checklist working

--- hotelReservation/src/app/cuisine-table/cuisine-table.component.ts
+++ hotelReservation/src/app/cuisine-table/cuisine-table.component.ts
@@ -20,6 +20,9 @@ export class CuisineTableComponent implements OnInit {
   @ViewChild(MatTableDataSource) filterData: MatTableDataSource<any>;
   dataSource: MatTableDataSource<any>;
   selection = new SelectionModel<any>(true, []);
+  selectedCuisines: any[] = [];
+  row_fake_tuples: any[] = [];
+  found: boolean = false;
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   //displayedColumns = ['cuisine_id', 'cuisine_name'];
   displayedColumns: string[] = ['select','cuisine_id', 'cuisine_name'];
@@ -46,11 +49,48 @@ export class CuisineTableComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
 
   }
+  checkBoxSelection(name_array : SelectionModel<any>){
+    console.log(name_array);
+    console.log(name_array.selected);
+    //console.log(name_array._selection);
+  }
+  selectedRow(row_original){
+    console.log(row_original);
+    row_original.isSelected = true;
+    this.found = false;
+    //loop through the list of selected cuisines to see if that 
+    //cuisine needs to be removed or added to the list
+    var i;
+    if(this.selectedCuisines.length != 0){
+      for(i = 0; i < this.selectedCuisines.length; i++){
+
+        //must remove that cuisine from the list here
+        console.log(this.selectedCuisines[i].cuisine_id);
+        console.log(row_original.cuisine_id);
+        if((this.selectedCuisines[i].cuisine_id == row_original.cuisine_id)){
+          //removes that element at that index
+          this.found = true;
+          
+          var index = this.selectedCuisines.findIndex(x=>x.cuisine_name == row_original.cuisine_name);
+          console.log(index);
+          this.selectedCuisines.splice(index,1);
+        }
+      }
+    }
+    if(this.found == false){
+      this.selectedCuisines.push(row_original);
+    }
+    //need to add the cuisine here
+    //when call that object has been selected so we attach a bool attribute
+    if(this.selectedCuisines.length==0){
+      this.selectedCuisines.push(row_original);
+    }
+    console.log(this.found);
+    
+    console.log(this.selectedCuisines);
+  }
+
   applyFilter(filterValue: string){
-    console.log('in the apply filter');
-    console.log(this.dataSource);
-    console.log(this.dataSource.paginator);
-      
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
