7d2bdf17b217ccd9dc6888f9fa0d2e128235555f
Kunal Shukla <kunal.shukla@intact.net>
2018-12-12 15:32:15 -0500

    refactoring UI

---
+++ hotelReservation/src/app/restaurant-table/dialog-open.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+
+
+@Component({
+    selector: 'app-restaurant-dialog',
+    templateUrl: './dialog-open.html',
+    styleUrls: ['./dialog-open.css']
+})
+export class DialogComponent implements OnInit{
+
+    ngOnInit(){
+
+    }
+}
